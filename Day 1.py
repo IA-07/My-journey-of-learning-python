13228b9052ebca92a3fdd07fd508eeb538fc53f8
IA <158739081+IA-07@users.noreply.github.com>
2024-02-09 20:03:25 +0600

    Add files via upload

---
+++ Day 1.py
@@ -0,0 +1,12 @@
+print("hello everyone!")
+print("Here is my intro-")
+first_name= "Iftekhar"
+last_name= "Anam"
+age= "28"
+is_adult= True
+print("My name is "+first_name+" "+last_name)
+print("I'm "+age+ " of old")
+print(is_adult)
+print("Okay, you can know more about me")
+user_name= input("What is your user name?")
+print(user_name)
